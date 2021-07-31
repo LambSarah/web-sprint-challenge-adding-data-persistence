a0f2c01952a54ca88390b95fea57a08c1a1f6bf6
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-30 23:16:07 -0500

    completed index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+const express = require('express')
+const recipesRouter = require('./recipes/recipes-router')
+const { errorHandling, notFound } = require('./recipes/recipes-middleware')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/recipes', recipesRouter)
+server.get('/', function (req, res, next) {
+	res.send('Welcome to Recipes API')
+	next()
+})
+
+server.use('*', notFound)
+server.use(errorHandling)
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// start your server here
+// start your server herec
+const server = require('./api/server')
+
+const port = process.env.PORT || 8800
+
+server.listen(port, () => console.log(`\n_____Running on port ${port}_____\n`))
